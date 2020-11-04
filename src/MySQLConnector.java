dd68ec3ea2d162c48326cd95d217ffaaa184a4ef
ami <amihaiwow@gmail.com>
2020-11-04 15:37:53 +0100

    rettelser

--- src/MySQLConnector.java
+++ src/MySQLConnector.java
@@ -3,11 +3,10 @@ import java.time.LocalDateTime;
 
 
 public class MySQLConnector {
-    static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
     static String url = "jdbc:mariadb://192.168.239.20:3306/myuser?";
     private static Connection conn = null;
-    private static Statement statement = null;
-    private PreparedStatement prep = null;
+    //private static Statement statement = null;
+    //private PreparedStatement prep = null;
 
     public static void main(String[] args) {
         LoginData loginData = new LoginData();
@@ -29,17 +28,6 @@ public class MySQLConnector {
                 System.out.println("connection not made");
             }
 
-            //find out which columns are in current table:
-            /*ResultSetMetaData rsMetaData = rs.getMetaData();
-            int numberOfColumns = rsMetaData.getColumnCount();
-
-            // get the column names; column indexes start from 1
-            for (int i = 1; i < numberOfColumns + 1; i++) {
-                String columnName = rsMetaData.getColumnName(i);
-                // Get the name of the column's table name
-                String tableName = rsMetaData.getTableName(i);
-                System.out.println("column name=" + columnName);
-            }*/
 
 
             //db.getHomeData();
