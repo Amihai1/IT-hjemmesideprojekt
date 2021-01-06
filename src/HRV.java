bc7775317060e5d86b9eb237e1d90799de61afbb
ami <amihaiwow@gmail.com>
2021-01-06 22:49:45 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/HTML/Login.html

--- src/HRV.java
+++ src/HRV.java
@@ -10,27 +10,29 @@ public class HRV {
             double alle = 0;
             double gennemsnit;
             double gennem = 0;
-            double hej = 0;
+            int hej = 375;
+            int standardR = 600;
+            int randomR = 200;
+            int randomRG= 200;
 
-            double[] gen = new double[375];
-            double[] puls = new double[375];
-            for (int n = 0; n < 375; n++) {
-                gennemsnit = 750 + Math.random() * 300;
+            double[] gen = new double[hej];
+            double[] puls = new double[hej];
+            for (int n = 0; n < hej; n++) {
+                gennemsnit = standardR + Math.random() * randomR + Math.random()*randomR;
                 gen[n] = gennemsnit;
                 gennem += gen[n];
             }
-            double gennesnitsHRV = gennem / 375;
+            double gennesnitsHRV = gennem / hej;
 
-            for (int n = 0; n < 375; n++) {
-                double abe = 750 + Math.random() * 300;
+            for (int n = 0; n < hej; n++) {
+                double abe = standardR + Math.random() * randomR+ Math.random()*randomR;
                 puls[n] = abe;
-                hej = puls[n];
                 alle += Math.pow(puls[n] - gennesnitsHRV, 2);
             }
 
 
             //System.out.println(Arrays.toString(puls));
-            double beregn = Math.sqrt(alle / (375 - 1));
+            double beregn = Math.sqrt(alle / (hej - 1));
             log += beregn;
 
             //double hr = 1/beregn*60;
